bae22aee52186fe249b6ba60fd85c925a3c29d90
ThomazPom <thomas.benhamou@hotmail.fr>
2022-08-28 12:06:42 +0200

    Various improvements including pdf support

--- main.js
+++ main.js
@@ -28,54 +28,61 @@ const settings = {
 }
 function loadPersistentSettings()
 {
-
-const settingsFile = path.join(__dirname,"settings.yaml");
-try {
-    const doc = yaml.load(fs.readFileSync(settingsFile), 'utf8');
     
-    settings.persistentSettings = doc
-    
-    console.log("Loaded settings")
-  } catch (e) {
-      settings.persistentSettings = {
-          replay_blacklist_regex:{
-            "any":[],
-            "example.com":[
-                "http://example.com/complexRequest###i",
-                "https?://example.com/some(.*?)",
-              ],
-            "never_replay.org":"*"
-          }
-
+    const settingsFile = path.join(__dirname,"settings.yaml");
+    try {
+        const doc = yaml.load(fs.readFileSync(settingsFile), 'utf8');
+        
+        settings.persistentSettings = doc
+        
+        console.log("Loaded settings")
+    } catch (e) {
+        settings.persistentSettings = {
+            replay_blacklist_regex:{
+                "any":[],
+                "example.com":[
+                    "http://example.com/complexRequest###i",
+                    "https?://example.com/some(.*?)",
+                ],
+                "never_replay.org":"*"
+            }
+            
+        }
+        const doc= yaml.dump(settings.persistentSettings);
+        if(!fs.existsSync(settingsFile))
+        {
+            fs.writeFile(settingsFile,doc,()=>{})
+        }
+        console.log("New settings");
     }
-    const doc= yaml.dump(settings.persistentSettings);
-    if(!fs.existsSync(settingsFile))
-    {
-        fs.writeFile(settingsFile,doc,()=>{})
-    }
-    console.log("New settings");
-  }
-  return settings.persistentSettings
+    return settings.persistentSettings
 }
 loadPersistentSettings();
 async function registerpage(page) {
+    
     if (!page) {
         return
     }
-        
+    
     const mainframe= await page.mainFrame()
     if(mainframe){
         console.log("Registering page", await page.url())
     }
     if (settings.mode_record) {
-
+        
         console.log('Recording new page', settings.mode_record);
         page.on('response', interceptorResponse);
         if(settings.enable_scripts)
         {
-            page.on('framenavigated', (frame) =>{
-                replay_record_functions.loader(frame,replay_record_functions.record_functions)
-            });
+            try{
+                page.on('framenavigated', (frame) =>{
+                    replay_record_functions.loader(frame,replay_record_functions.record_functions)
+                });
+            }
+            catch(err)
+            {
+                console.error("replay_record_functions crashed with error",err)
+            }
         }
     }
     if (settings.mode_replay) {
@@ -84,15 +91,23 @@ async function registerpage(page) {
         page.on('request', interceptorRequest);
         if(settings.enable_scripts)
         {
-            page.on('framenavigated', (frame) =>{
-                replay_record_functions.loader(frame,replay_record_functions.replay_functions)
-            });
+            try{
+                
+                page.on('framenavigated', (frame) =>{
+                    replay_record_functions.loader(frame,replay_record_functions.replay_functions)
+                });
+                
+            }
+            catch(err)
+            {
+                console.error("replay_record_functions crashed with error",err)
+            }
         }
     }
 }
 (async () => {
     
-
+    
     let launchArgs = {
         headless: false, defaultViewport: null,
         ignoreHTTPSErrors: settings.ignore_ssl_errors,
@@ -113,15 +128,15 @@ async function registerpage(page) {
         launchArgs.executablePath='/Applications/Google Chrome.app/Contents/MacOS/Google Chrome'
     }
     if (!settings.blank_profile) {
-
+        
         let userDataDir = path.join(__dirname, "profile")
         fs.mkdir(userDataDir, { recursive: true }, z => { })
         launchArgs.userDataDir = userDataDir
-
+        
     }
     const browser = await puppeteer.launch(launchArgs);
-
-
+    
+    
     console.log("BROWSER VERSION",await browser.version());
     console.log("PUPPETEER VERSION",process.versions);
     console.log("LAUNCH ARGS",launchArgs)
@@ -132,24 +147,30 @@ async function registerpage(page) {
     console.log("Grab from web missing files", settings.fetch_missing_from_web)
     let page0 = (await browser.pages())[0]
     await page0.goto("chrome://settings/cookies");
-
-        await page0.evaluate((selector) =>
-            document
-                .querySelector('settings-ui')
-                .shadowRoot.querySelector('settings-main')
-                .shadowRoot.querySelector('settings-basic-page')
-                .shadowRoot.querySelector('settings-section settings-privacy-page')
-                .shadowRoot.querySelector('settings-cookies-page')
-                .shadowRoot.querySelector(selector)
-                .shadowRoot.querySelector('#label')
-                .click()
-        ,settings.disable_cookies?"#blockAll":"#blockThirdPartyIncognito");
     
-    registerpage(page0)
+    await page0.evaluate((selector) =>
+    document
+    .querySelector('settings-ui')
+    .shadowRoot.querySelector('settings-main')
+    .shadowRoot.querySelector('settings-basic-page')
+    .shadowRoot.querySelector('settings-section settings-privacy-page')
+    .shadowRoot.querySelector('settings-cookies-page')
+    .shadowRoot.querySelector(selector)
+    .shadowRoot.querySelector('#label')
+    .click()
+    ,settings.disable_cookies?"#blockAll":"#blockThirdPartyIncognito");
+    
+    //registerpage(page0)
     browser.on('targetcreated', async function (tab) {
         console.log('New Tab Created');
-        let page = await tab.page()
-        registerpage(page)
+        try{
+            let page = await tab.page()
+            registerpage(page)
+        }
+        catch(err)
+        {
+            console.log("registration failed with error",err)
+        }
     })
 })();
 function isABlacklistReplay(parseurl){
@@ -163,24 +184,24 @@ function isABlacklistReplay(parseurl){
         }
         if(blacklist)
         {
-                for (const rx_string of blacklist) {
-                    //console.log(rx_string);
-                    let regex=new RegExp(...rx_string.split("###"))
-                   // console.log(regex)
-                    if(regex.test(parseurl.href))
-                    {
-                        console.log(parseurl.href,"is blacklisted by ",regex,"on",parseurl.hostname)
-                        return true
-                    }
+            for (const rx_string of blacklist) {
+                //console.log(rx_string);
+                let regex=new RegExp(...rx_string.split("###"))
+                // console.log(regex)
+                if(regex.test(parseurl.href))
+                {
+                    console.log(parseurl.href,"is blacklisted by ",regex,"on",parseurl.hostname)
+                    return true
                 }
-    //      console.log(parseurl.hostname,"has a blacklist")
+            }
+            //      console.log(parseurl.hostname,"has a blacklist")
             
         }
     }
     return false
 }
 async function interceptorRequest(request) {
-
+    
     var [parseurl, sitefolder, basename, filename, metadata_filename] = generateFilenames(request)
     if (fs.existsSync(metadata_filename) && !isABlacklistReplay(parseurl)) {
         let metadata = JSON.parse(fs.readFileSync(metadata_filename))
@@ -190,7 +211,7 @@ async function interceptorRequest(request) {
         //     request_method:response.request().method(),
         //     response_headers:responseHeaders,
         // })
-
+        
         console.log("REPLAY ", request.method(), request.url(), metadata.status);
         let response = {
             status: metadata.status,
@@ -203,12 +224,12 @@ async function interceptorRequest(request) {
     }
     else if (settings.fetch_missing_from_web) {
         console.log("FETCH ", request.method(), request.url());
-
+        
         request.continue();
     }
     else {
         console.log("SORRY ", request.method(), request.url(), "404");
-
+        
         request.respond({
             status: 404,
             headers: {
@@ -224,86 +245,106 @@ function generateFilenames(request) {
     var basename = sha1(request.method() + request.url() + request.postData())
     var filename = path.join(sitefolder, basename + '_' + parseurl.protocol.replace(":", ""));
     var metadata_filename = filename + "-res-metadata.json";
-
+    
     return [parseurl, sitefolder, basename, filename, metadata_filename]
 }
 async function interceptorResponse(response) {
-
-    let request = response.request();
-    let responseHeaders = response.headers();
-    console.log(request.method(), response.status(), request.url());
-
-    var [parseurl, sitefolder, basename, filename, metadata_filename] = generateFilenames(request)
-    if (settings.keep_local && fs.existsSync(metadata_filename)) {
-        console.log("Keeping local file", request.method(), response.status(), request.url())
-        return;
-    }
-    var metadata_json = JSON.stringify({
-        status: response.status(),
-        request_url: response.request().url(),
-        request_method: response.request().method(),
-        response_headers: responseHeaders,
-    })
-
-    fs.mkdir(sitefolder, { recursive: true }, z => { })
-
-    fs.writeFile(metadata_filename, metadata_json, (err) => {
-       // console.log(err)
-    })
-
-
-    var link_descriptor_filename = path.join(sitefolder, "known_links.txt");
-    let body_buffer=null;
-    if(responseHeaders["content-type"] && responseHeaders["content-type"].includes("application/pdf")
-    // || insert incompatible files here
     
-    )
+    try
     {
-        console.log("PDF HERE !",filename);
-        let request=response.request();
-
-        let fetchresult = await fetch( 
-            request.url(),
-            {
-                method:request.method(),
-                body:request.postData(),
-                headers:request.headers()
-            }
-        )
-
-        let body_arraybuffer = await fetchresult.arrayBuffer();
-        body_buffer =  Buffer.from(body_arraybuffer)
         
-    }
-
-    if(!body_buffer)
-    {
-        try{
+        let request = response.request();
+        let responseHeaders = response.headers();
+        if(request.isNavigationRequest())
+        {
+            console.log("################# NAVIGATON REQUEST #################")
+        }
+        console.log(request.method(), response.status(), request.url());
+        
+        var [parseurl, sitefolder, basename, filename, metadata_filename] = generateFilenames(request)
+        if (settings.keep_local && fs.existsSync(metadata_filename)) {
+            console.log("Keeping local file", request.method(), response.status(), request.url())
+            return;
+        }
+        var metadata_json = JSON.stringify({
+            status: response.status(),
+            request_url: response.request().url(),
+            request_method: response.request().method(),
+            response_headers: responseHeaders,
+        })
+        
+        fs.mkdir(sitefolder, { recursive: true }, z => { })
+        
+        fs.writeFile(metadata_filename, metadata_json, (err) => {
+            // console.log(err)
+        })
+        
+        
+        var link_descriptor_filename = path.join(sitefolder, "known_links.txt");
+        let body_buffer=null;
+        if(responseHeaders["content-type"] && responseHeaders["content-type"].includes("application/pdf")
+        // || insert incompatible files here
+        
+        )
+        {
+            try
+            {
+                console.log("PDF HERE !",filename);
+                let request=response.request();
+                
+                let fetchresult = await fetch( 
+                    request.url(),
+                    {
+                        method:request.method(),
+                        body:request.postData(),
+                        headers:request.headers()
+                    }
+                    )
+                    
+                    let body_arraybuffer = await fetchresult.arrayBuffer();
+                    body_buffer =  Buffer.from(body_arraybuffer)
+                }
+                catch(err)
+                {
+                    console.log(
+                        "PDF loading failed",err)
+                    }
+                    
+                }
+                
+                if(!body_buffer)
+                {
+                    try{
+                        
+                        body_buffer = await response.buffer();
+                    }
+                    catch(err){
+                        
+                        console.log("This request did not have a body", err.message)
+                        return // No body, no file to write. This can happen on rediredted responses 
+                    }
+                }
+                
+                if(!body_buffer) // idk how we can end up in this situation but .. just in case
+                {
+                    console.error("Not writing data cause buffer is empty")
+                    body_buffer = Buffer.from("No data found for this request even with fetch")
+                }
+                
+                
+                if (responseHeaders["content-type"] && responseHeaders["content-type"].includes("text/html")) {
+                    if (!fs.existsSync(filename)) {
+                        fs.appendFile(link_descriptor_filename, parseurl.href + "\n", (err) => {
+                            // console.log(error,link_descriptor_filename,parseurl.href)
+                        });
+                    }
+                }
+                fs.writeFile(filename, body_buffer, () => {})
+                
+            }
+            catch(err){
+                console.log("request interception failed with error",err)
+                process.exit();
+            }
             
-            body_buffer = await response.buffer();
         }
-        catch(err){
-            
-            console.log("This request did not have a body", err.message)
-            return // No body, no file to write. This can happen on rediredted responses 
-        }
-    }
-
-    if(!body_buffer) // idk how we can end up in this situation but .. just in case
-    {
-        console.error("Not writing data cause buffer is empty")
-        body_buffer = Buffer.from("No data found for this request even with fetch")
-    }
-
-    
-    if (responseHeaders["content-type"] && responseHeaders["content-type"].includes("text/html")) {
-        if (!fs.existsSync(filename)) {
-            fs.appendFile(link_descriptor_filename, parseurl.href + "\n", (err) => {
-                // console.log(error,link_descriptor_filename,parseurl.href)
-            });
-        }
-    }
-    fs.writeFile(filename, body_buffer, () => {})
-
-    
-}
--- replay_record_functions.js
+++ replay_record_functions.js
@@ -1,56 +1,15 @@
-
-
-
-function enable_all_buttons()
-{
-    setInterval(z=>{
-            document.querySelectorAll("[disabled]").forEach(z=>{
-                z.removeAttribute("disabled")
-            })
-    },1000)
-}
-function autoclickOnSomeSelectors()
-{
-    let selectors= [
-        '#course.end .nextslide.custback',    
-        'button[data-slide="next"]'
-    ].join(",")|| "noselector";
-    let disablerSelectors=  [
-        //"#course.end"
-    ].join(",") || "noselector";
-    let disablerHtmls=  [`Sortir <i class="fa fa-share"></i></a>`]
-    setInterval(     z=>   {
-        if(document.querySelector(disablerSelectors))
-        {
-            return;
-        }
-        for (let i = 0; i < disablerHtmls.length; i++) {
-            const element = disablerHtmls[i];
-            if(document.documentElement.innerHTML.includes(element))
-            {
-                return;
-            }   
-        }
-        
-        let selectResult = document.querySelector(selectors);
-        if(selectResult)
-        {
-            selectResult.click();
-        }
-    }
-    ,2000)
-}
-
 function say_hello()
 {
     console.log("Hello World","from",document.location.host,document.location.pathname)
 }
 
+
+
 let record_functions= [
-    enable_all_buttons,autoclickOnSomeSelectors
+    say_hello
 ];
 let replay_functions= [
-    enable_all_buttons
+    say_hello
 ];
 
 function loader(frame,functions){
